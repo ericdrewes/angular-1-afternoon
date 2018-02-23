59b5e1004503a0a87b61cf58a00469b6edef855b
ericdrewes <eric.d.drewes@gmail.com>
2018-02-23 14:03:33 -0600

    update

--- js/friendCtrl.js
+++ js/friendCtrl.js
@@ -1,15 +1,13 @@
-angular.module("myApp").controller(friendCtrl, function($scope) {
-    $scope.friends = [];
-
-    $scope.filter = {
-        name:'',
-        state:''
-    }
-
+angular.module("myApp").controller("friendCtrl", function( $scope ) {
+    $scope.friends = // array from mock-data.json
+  
+    $scope.filters = {
+      name: '',
+      state: ''
+    };
+  
     $scope.sorts = {
-        property = "name"
-        direction = "+"
-    }
-
-
-})
+      property: 'name',
+      direction: '+'
+    };
+  });
